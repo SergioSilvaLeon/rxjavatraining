7b0b28c0b84cadb946dc2f519cbd4219818cc357
Sergio Silva <ssilva@nearsoft.com>
2018-05-02 17:00:54 -0700

    Initial commit

---
+++ app/src/main/java/com/donnfelker/rxjavatraining/MainActivity.java
@@ -0,0 +1,43 @@
+package com.donnfelker.rxjavatraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.Callable;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable
+                .fromCallable(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        wait(5 * 1000);
+                        return "Hello World!";
+                    }
+                })
+                .subscribeOn(Schedulers.computation())
+                .observeOn(Schedulers.io())
+                .doOnNext(s -> saveToDb(s))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(message ->
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                );
+
+
+    }
+
+    private void saveToDb(String s) {
+        // Save to Database
+    }
+}
